8be8f5bac5eca3905e2d2cc9b9c6c8026be3f5bf
andriel300 <andrielscript@gmail.com>
2023-02-21 20:57:56 -0300

    updated the styles

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-// Get the container div
-const container = document.getElementById('container');
+document.addEventListener('DOMContentLoaded', function () {
+  // Get the container div
+  const container = document.getElementById('container');
 
-// Create a 16x16 grid of square divs using nested loop to create rows
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    square.style.width = '40px';
-    square.style.height = '40px';
-    container.appendChild(square);
+  // Create a 16x16 grid of square divs using nested loop to create rows
+  for (let i = 0; i < 16; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+
+    for (let j = 0; j < 16; j++) {
+      const square = document.createElement('div');
+      square.classList.add('square');
+      row.appendChild(square);
+    }
+    container.appendChild(row);
   }
-}
+});
