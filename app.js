355a6efa2233f9fb5a76250522fbb9c1eb893e73
andriel300 <andrielscript@gmail.com>
2023-02-22 08:37:06 -0300

    add slider and value output

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const grid = document.querySelector('.grid');
 
+// Create div rows, columns.
 const createGrid = () => {
-  for (let i = 0; i < 16 * 16; i++) {
+  for (let i = 0; i < 256; i++) {
     const square = document.createElement('div');
     square.addEventListener('mouseover', () => {
       square.style.backgroundColor = 'black';
@@ -9,9 +10,18 @@ const createGrid = () => {
     grid.appendChild(square);
   }
 
-  container.style.display = 'grid';
-  container.style.gridTemplateColumns = 'repeat(16, 1fr)';
-  container.style.gridTemplateRows = 'repeat(16, 1fr)';
+  grid.style.display = 'grid';
+  grid.style.gridTemplateColumns = 'repeat(16, 1fr)';
+  grid.style.gridTemplateRows = 'repeat(16, 1fr)';
+
+  // Slider
+  const slider = document.getElementById('myRange');
+  const output = document.getElementById('outputValue');
+  output.innerHTML = slider.value;
+
+  slider.oninput = function () {
+    output.innerHTML = this.value;
+  };
 };
 
 createGrid();
