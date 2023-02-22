06fa1378664bf6dc0b7d541700ab98a1adcdf345
andriel300 <andrielscript@gmail.com>
2023-02-22 11:47:49 -0300

    fix bug where you cant adjust the size of the slider range

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 const grid = document.querySelector('.grid');
 
 // Create div rows, columns.
-const createGrid = (size) => {
-  // Remove all child nodes from the grid
-  while (grid.firstChild) {
-    grid.removeChild(grid.lastChild);
-  }
-
+const createGrid = (size = 16) => {
   // Calculate the number of squares per row and colum
-  const squaresPerSide = size;
-  const totalSquares = squaresPerSide * squaresPerSide;
 
-  for (let i = 0; i < totalSquares; i++) {
+  for (let i = 0; i < size * size; i++) {
     const square = document.createElement('div');
     square.addEventListener('mouseover', () => {
       square.style.backgroundColor = 'black';
@@ -21,15 +14,28 @@ const createGrid = (size) => {
 
   // Set the grid styles
   grid.style.display = 'grid';
-  grid.style.gridTemplateColumns = `repeat(${squaresPerSide}, 1fr)`;
-  grid.style.gridTemplateRows = `repeat(${squaresPerSide}, 1fr)`;
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
   // Slider
   const slider = document.getElementById('myRange');
   const output = document.getElementById('outputValue');
-  output.innerHTML = slider.value;
-  slider.value = size;
   output.innerHTML = size;
+
+  slider.oninput = function () {
+    const newSize = this.value;
+    output.innerHTML = newSize;
+
+    // Set the size of the squares between 1px and 64px
+    const squareSize = Math.min(64, Math.max(1, Math.floor(640 / newSize))) + 'px';
+
+    // Remove all existing squares from the grid
+    grid.innerHTML = '';
+    // Create the new grid with the specified size and square size
+    grid.style.gridTemplateColumns = `repeat(${newSize}, ${squareSize})`;
+    grid.style.gridTemplateRows = `repeat(${newSize}, ${squareSize})`;
+    createGrid(newSize);
+  };
 };
 
-createGrid(16);
+createGrid();
