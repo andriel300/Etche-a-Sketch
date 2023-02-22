87d8a69a6a848059449d06cd2ef6814829a6a9d5
andriel300 <andrielscript@gmail.com>
2023-02-21 21:48:44 -0300

    add arrow function to createGrid on js

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // Get the container div
-  const container = document.getElementById('container');
+const grid = document.querySelector('.grid');
 
-  // Create a 16x16 grid of square divs using nested loop to create rows
-  for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-
-    for (let j = 0; j < 16; j++) {
-      const square = document.createElement('div');
-      square.classList.add('square');
-      row.appendChild(square);
-    }
-    container.appendChild(row);
+const createGrid = () => {
+  for (let i = 0; i < 16 * 16; i++) {
+    const square = document.createElement('div');
+    square.addEventListener('mouseover', () => {
+      square.style.backgroundColor = 'black';
+    });
+    grid.appendChild(square);
   }
-});
+
+  container.style.display = 'grid';
+  container.style.gridTemplateColumns = 'repeat(16, 1fr)';
+  container.style.gridTemplateRows = 'repeat(16, 1fr)';
+};
+
+createGrid();
