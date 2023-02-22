ce83cf034a72d82d318ac4d2aee11080cb6d836c
andriel300 <andrielscript@gmail.com>
2023-02-22 12:03:19 -0300

    add cleaner comments line again

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const grid = document.querySelector('.grid');
 // Create div rows, columns.
 const createGrid = (size = 16) => {
   // Calculate the number of squares per row and colum
-  const totalSquares = size * size;
+  const numSquares = size * size;
 
   // Loop through and create a new square for each
-  for (let i = 0; i < totalSquares; i++) {
+  for (let i = 0; i < numSquares; i++) {
     const square = document.createElement('div');
     // add an event listener to change the square color on hover
     square.addEventListener('mouseover', () => {
@@ -22,22 +22,17 @@ const createGrid = (size = 16) => {
   grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-  // Select the slider and its output element
+  // Initialize the slider
   const slider = document.getElementById('myRange');
   const output = document.getElementById('outputValue');
-
-  // Set the initial output value to the grid size
   output.innerHTML = size;
 
-  // Add an input event listener to update the grid size when the slider is moved
+  // Handle the slider input event
   slider.oninput = function () {
     const newSize = this.value;
-
-    // Update the output value
     output.innerHTML = newSize;
 
     // Set the size of the squares between 1px and 64px
-    // Calculate the size of each square based on the grid size
     const squareSize = Math.min(64, Math.max(1, Math.floor(640 / newSize))) + 'px';
 
     // Remove and clear all existing squares from the grid
