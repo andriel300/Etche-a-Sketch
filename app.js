00f98d90e568e3ed1d9fe3b6e806b9d2c6c6e979
andriel300 <andrielscript@gmail.com>
2023-02-22 11:23:00 -0300

    add size pixels squares aaccording to adjusment on function createGrid

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const grid = document.querySelector('.grid');
 
 // Create div rows, columns.
-const createGrid = () => {
-  for (let i = 0; i < 256; i++) {
+const createGrid = (size) => {
+  // Remove all child nodes from the grid
+  while (grid.firstChild) {
+    grid.removeChild(grid.lastChild);
+  }
+
+  // Calculate the number of squares per row and colum
+  const squaresPerSide = size;
+  const totalSquares = squaresPerSide * squaresPerSide;
+
+  for (let i = 0; i < totalSquares; i++) {
     const square = document.createElement('div');
     square.addEventListener('mouseover', () => {
       square.style.backgroundColor = 'black';
@@ -10,18 +19,17 @@ const createGrid = () => {
     grid.appendChild(square);
   }
 
+  // Set the grid styles
   grid.style.display = 'grid';
-  grid.style.gridTemplateColumns = 'repeat(16, 1fr)';
-  grid.style.gridTemplateRows = 'repeat(16, 1fr)';
+  grid.style.gridTemplateColumns = `repeat(${squaresPerSide}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${squaresPerSide}, 1fr)`;
 
   // Slider
   const slider = document.getElementById('myRange');
   const output = document.getElementById('outputValue');
   output.innerHTML = slider.value;
-
-  slider.oninput = function () {
-    output.innerHTML = this.value;
-  };
+  slider.value = size;
+  output.innerHTML = size;
 };
 
-createGrid();
+createGrid(16);
